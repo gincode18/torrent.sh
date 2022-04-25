de10a5dfd69507b1412662321e01988a88de6339
gincode18 <91776192+gincode18@users.noreply.github.com>
2022-04-25 23:19:18 +0530

    new script
    jskhbd

--- torrent.sh
+++ torrent.sh
@@ -6,6 +6,26 @@ n="%20"
 result=$(echo $query | sed "s/$o/$n/g")
 magnet=$(curl https://thepiratebay.party/search/$result/1/99/0 | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" | head -n 1)
 
-webtorrent $magnet --mpv --playlist
-
+curl https://thepiratebay.party/search/$result/1/99/0 | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" | head -n 5 > txt
+cat txt
+echo "enter the magnet no from 1-5"
+read number
+if [ $number -eq 1 ]
+then
+    torrent=$(cat txt | sed -n ' 1 p')
+elif [ $number -eq 2 ] 
+then
+    torrent=$(cat txt | sed -n ' 2 p')
+elif [ $number -eq 3 ] 
+then
+    torrent=$(cat txt | sed -n ' 3 p')
+elif [ $number -eq 4 ] 
+then
+    torrent=$(cat txt | sed -n ' 4 p')
+elif [ $number -eq 5 ] 
+then
+    torrent=$(cat txt | sed -n ' 5 p')
+    
+fi
+webtorrent $torrent --mpv --playlist
 
